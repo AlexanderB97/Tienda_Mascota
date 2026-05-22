e49eaf2b95ef7bb64e46f16bd3942896d6684ee6
alex <alexanderbenitezrivpla97@gmail.com>
2026-05-22 16:47:13 -0300

    feat: agrega controlador, vistas y migraciones de productos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductoControllers;
+Route::get('/productos', [ProductoControllers::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
